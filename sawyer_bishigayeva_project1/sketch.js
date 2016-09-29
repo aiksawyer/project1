dc45282389359eb93a46ff91fd8550d375ed98f6
aiksawyer <abishigayeva@gmail.com>
2016-09-29 13:59:49 -0400

    Add files via upload
    General outline of project

---
+++ sawyer_bishigayeva_project1/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  
+}
+
+function draw() {
+  /*ADJECTIVE ––– SORE
+  
+  a figure (stick figure??) is fired up for movement
+  
+  various animations of exercise – jumping through other objects, running
+  show movement??? scenery passing by, lines of movement
+  
+  figure gets slower and slower until stop
+  red pointy figures to indicate pain/ blue flowy figures for fatigue
+  spend more time on this part – how soreness impedes movement? visual expression
+  
+  relaxation? stretching movements, peaceful scenery, water??
+  
+  figure goes to sleep ??? show sleep – nighttime, peace, unchanging scenery
+  
+  wakes up happy and ready to move again
+  */
+  
+}
+
+///checking git app
